b4978c4334e1fbc571ec740e9798a70a4abd7a6b
MoeFH <ricardo.massaro@gmail.com>
2025-03-02 00:00:42 -0300

    Make it work on mobile

--- temperament/main.js
+++ temperament/main.js
@@ -12,32 +12,32 @@ class NoteNames {
     getOrder() { return this.order; }
     
     getAllNotes() {
-	return Array.from({length: 12}, (_, i) => this.getName(i));
+        return Array.from({length: 12}, (_, i) => this.getName(i));
     }
     
     getName(pos) {
-	if (this.order == 'fifths') {
-	    return NoteNames.fifthNames[(pos * 7 + this.root) % 12];
-	}
-	return NoteNames.semitoneNames[(pos + this.root) % 12];
+        if (this.order == 'fifths') {
+            return NoteNames.fifthNames[(pos * 7 + this.root) % 12];
+        }
+        return NoteNames.semitoneNames[(pos + this.root) % 12];
     }
 }
 
 class IntervalNames {
 
     static names = [
-	{ name: 'P1', desc: 'unison' },
-	{ name: 'm2', desc: 'minor second' },
-	{ name: 'M2', desc: 'major second' },
-	{ name: 'm3', desc: 'minor third' },
-	{ name: 'M3', desc: 'major third' },
-	{ name: 'P4', desc: 'perfect fourth' },
-	{ name: 'TT', desc: 'tritone' },
-	{ name: 'P5', desc: 'perfect fifth' },
-	{ name: 'm6', desc: 'minor sixth' },
-	{ name: 'M6', desc: 'major sixth' },
-	{ name: 'm7', desc: 'minor seventh' },
-	{ name: 'M7', desc: 'major seventh' },
+        { name: 'P1', desc: 'unison' },
+        { name: 'm2', desc: 'minor second' },
+        { name: 'M2', desc: 'major second' },
+        { name: 'm3', desc: 'minor third' },
+        { name: 'M3', desc: 'major third' },
+        { name: 'P4', desc: 'perfect fourth' },
+        { name: 'TT', desc: 'tritone' },
+        { name: 'P5', desc: 'perfect fifth' },
+        { name: 'm6', desc: 'minor sixth' },
+        { name: 'M6', desc: 'major sixth' },
+        { name: 'm7', desc: 'minor seventh' },
+        { name: 'M7', desc: 'major seventh' },
     ];
 
     static name(n) { return IntervalNames.names[n].name; }
@@ -71,51 +71,55 @@ function set_temperament()
 class Tempering {
 
     constructor() {
-	this.canvasElement = document.getElementById('circle-canvas');
-	this.intervalElement = document.getElementById('interval');
-	this.temperament = new EqualTemperament();
-	this.notes = new NoteNames();
-	this.circle = new CircleDisplay(this.canvasElement);
-	this.circle.setHandler('intervalChanged', (p1, p2) => this.intervalChanged(p1, p2));
+        this.canvasElement = document.getElementById('circle-canvas');
+        this.temperament = new EqualTemperament();
+        this.notes = new NoteNames();
+        this.circle = new CircleDisplay(this.canvasElement);
+        this.circle.setHandler('intervalChanged', (p1, p2) => this.intervalChanged(p1, p2));
     }
 
     setRootNote(root) {  // 0 = C, 1 = C#, ...
-	this.notes.setRoot(root);
-	this.circle.setNoteNames(this.notes.getAllNotes());
-	this.circle.render();
+        this.notes.setRoot(root);
+        this.circle.setNoteNames(this.notes.getAllNotes());
+        this.circle.render();
     }
     
     setNoteOrder(order) {
-	this.notes.setOrder(order);
-	this.circle.setNoteNames(this.notes.getAllNotes());
-	this.circle.render();
+        this.notes.setOrder(order);
+        this.circle.setNoteNames(this.notes.getAllNotes());
+        this.circle.render();
     }
 
     setTemperament(temperament) {
-	this.temperament = temperament;
+        this.temperament = temperament;
     }
     
     intervalChanged(pos1, pos2) {
-	if (pos1 === null) {
-	    this.intervalElement.innerText = '';
-	    return;
-	}
+        if (pos1 === null || pos2 === null) {
+            //this.intervalElement.innerText = '';
+            return;
+        }
 
-	let name1 = this.notes.getName(pos1);
-	let name2 = this.notes.getName(pos2);
+        let name1 = this.notes.getName(pos1);
+        let name2 = this.notes.getName(pos2);
 
-	let noteOrder = this.notes.getOrder();
-	let note1 = (noteOrder == 'fifths') ? pos1 * 7 % 12 : pos1;
-	let note2 = (noteOrder == 'fifths') ? pos2 * 7 % 12 : pos2;
-	let semitones = (note2 - note1 + 12) % 12;
-	let name = IntervalNames.name(semitones);
-	let desc = IntervalNames.desc(semitones);
+        let noteOrder = this.notes.getOrder();
+        let note1 = (noteOrder == 'fifths') ? pos1 * 7 % 12 : pos1;
+        let note2 = (noteOrder == 'fifths') ? pos2 * 7 % 12 : pos2;
+        let semitones = (note2 - note1 + 12) % 12;
+        let name = IntervalNames.name(semitones);
+        let desc = IntervalNames.desc(semitones);
 
-	let interval = this.temperament.getInterval(note1, note2);
-	let ratio = formatNumber(interval.value, 4);
-	let cents = formatNumber(1200 * Math.log2(interval.value), 2);
-	let label = (interval.label !== '') ? `(${interval.label})` : '';
-	this.intervalElement.innerText = `${name1} - ${name2}\n${name}: ${desc}\nRatio: ${ratio} ${label}\nCents: ${cents}`;
+        let interval = this.temperament.getInterval(note1, note2);
+        let ratio = formatNumber(interval.value, 4);
+        let cents = formatNumber(1200 * Math.log2(interval.value), 2);
+        let label = (interval.label !== '') ? `(${interval.label})` : '';
+        //this.intervalElement.innerText = `${name1} - ${name2}\n${name}: ${desc}\nRatio: ${ratio} ${label}\nCents: ${cents}`;
+        this.circle.renderInfo(`${name1} - ${name2}\n${name}: ${desc}\nRatio: ${ratio} ${label}\nCents: ${cents}`);
+    }
+
+    resize() {
+        this.circle.resize();
     }
 }
 
@@ -125,4 +129,10 @@ document.addEventListener('DOMContentLoaded', function() {
     set_root_note();
     set_note_order();
     set_temperament();
+}, false);
+
+window.addEventListener('resize', function() {
+    if (tempering) {
+        tempering.resize();
+    }
 }, false);
--- temperament/temperaments.js
+++ temperament/temperaments.js
@@ -10,50 +10,50 @@ function formatNumber(n, digits)
 class Fraction {
 
     constructor(n, d) {
-	this.n = (n === undefined) ? 0 : n;
-	this.d = (d === undefined) ? 1 : d;
+        this.n = (n === undefined) ? 0 : n;
+        this.d = (d === undefined) ? 1 : d;
     }
 
     clone() {
-	return new Fraction(this.n, this.d);
+        return new Fraction(this.n, this.d);
     }
     
     getValue() {
-	return this.n / this.d;
+        return this.n / this.d;
     }
 
     getLabel() {
-	return `${this.n}/${this.d}`;
+        return `${this.n}/${this.d}`;
     }
 
     multiply(n, d) {
-	if ((n instanceof Fraction) && d === undefined) {
-	    this.multiply(n.n, n.d);
-	} else {
-	    this.n *= n;
-	    if (d !== undefined) this.d *= d;
-	    this.reduce();
-	}
+        if ((n instanceof Fraction) && d === undefined) {
+            this.multiply(n.n, n.d);
+        } else {
+            this.n *= n;
+            if (d !== undefined) this.d *= d;
+            this.reduce();
+        }
     }
 
     divide(n, d) {
-	if ((n instanceof Fraction) && d === undefined) {
-	    this.multiply(n.d, n.n);
-	} else {
-	    this.multiply((d === undefined) ? 1 : d, n);
-	}
+        if ((n instanceof Fraction) && d === undefined) {
+            this.multiply(n.d, n.n);
+        } else {
+            this.multiply((d === undefined) ? 1 : d, n);
+        }
     }
 
     reduce() {
-	let a = this.n;
-	let b = this.d;
-	while (b != 0) {
-	    let t = b;
-	    b = a % b;
-	    a = t;
-	}
-	this.n /= a;
-	this.d /= a;
+        let a = this.n;
+        let b = this.d;
+        while (b != 0) {
+            let t = b;
+            b = a % b;
+            a = t;
+        }
+        this.n /= a;
+        this.d /= a;
     }
     
 }
@@ -62,62 +62,62 @@ class Fraction {
 class TemperamentUtil {
 
     static scaleFromNumericFifth(fifth) {
-	// build scale from fundamental (1) plus 6 ascending fifths plus 5 descending fifths
-	let freqs = [1];
-	let up = 1;
-	let down = 1;
-	for (let i = 0; i < 6; i++) {
-	    up *= fifth;
-	    while (up >= 2) up /= 2;
-	    freqs.push(up);
-	    if (i == 5) continue;  // don't include descending tritone
-	    down /= fifth;
-	    while (down <= 1) down *= 2;
-	    freqs.push(down);
-	}
+        // build scale from fundamental (1) plus 6 ascending fifths plus 5 descending fifths
+        let freqs = [1];
+        let up = 1;
+        let down = 1;
+        for (let i = 0; i < 6; i++) {
+            up *= fifth;
+            while (up >= 2) up /= 2;
+            freqs.push(up);
+            if (i == 5) continue;  // don't include descending tritone
+            down /= fifth;
+            while (down <= 1) down *= 2;
+            freqs.push(down);
+        }
 
-	// sort list by value
-	freqs.sort((a, b) => a - b);
-	return freqs;
+        // sort list by value
+        freqs.sort((a, b) => a - b);
+        return freqs;
     }
 
     static scaleFromFractionalFifth(fifth) {
-	// build scale from fundamental (1) plus 6 ascending fifths plus 5 descending fifths
-	let freqs = [new Fraction(1)];
-	let up = freqs[0].clone();
-	let down = freqs[0].clone();
-	for (let i = 0; i < 6; i++) {
-	    up.multiply(fifth);
-	    while (up.getValue() >= 2) up.multiply(1, 2);
-	    freqs.push(up.clone());
-	    if (i == 5) continue;  // don't include descending tritone
-	    down.divide(fifth);
-	    while (down.getValue() <= 1) down.multiply(2);
-	    freqs.push(down.clone());
-	}
+        // build scale from fundamental (1) plus 6 ascending fifths plus 5 descending fifths
+        let freqs = [new Fraction(1)];
+        let up = freqs[0].clone();
+        let down = freqs[0].clone();
+        for (let i = 0; i < 6; i++) {
+            up.multiply(fifth);
+            while (up.getValue() >= 2) up.multiply(1, 2);
+            freqs.push(up.clone());
+            if (i == 5) continue;  // don't include descending tritone
+            down.divide(fifth);
+            while (down.getValue() <= 1) down.multiply(2);
+            freqs.push(down.clone());
+        }
 
-	// sort list by value
-	freqs.sort((a, b) => a.getValue() - b.getValue());
-	return freqs;
+        // sort list by value
+        freqs.sort((a, b) => a.getValue() - b.getValue());
+        return freqs;
     }
     
     static getNumericInterval(freqs, note1, note2) {
-	let interval = freqs[note2] / freqs[note1];
-	if (note2 < note1) interval *= 2;
-	return {
-	    value: interval,
-	    label: '',
-	};
+        let interval = freqs[note2] / freqs[note1];
+        if (note2 < note1) interval *= 2;
+        return {
+            value: interval,
+            label: '',
+        };
     }
 
     static getFractionalInterval(freqs, note1, note2) {
-	let interval = freqs[note2].clone();
-	if (note2 < note1) interval.multiply(2);
-	interval.divide(freqs[note1]);
-	return {
-	    value: interval.getValue(),
-	    label: interval.getLabel(),
-	};
+        let interval = freqs[note2].clone();
+        if (note2 < note1) interval.multiply(2);
+        interval.divide(freqs[note1]);
+        return {
+            value: interval.getValue(),
+            label: interval.getLabel(),
+        };
     }
 
 }
@@ -125,22 +125,22 @@ class TemperamentUtil {
 class EqualTemperament {
 
     constructor() {
-	this.freqs = Array.from({length:12}, (_, i) => Math.pow(2, i/12));
+        this.freqs = Array.from({length:12}, (_, i) => Math.pow(2, i/12));
     }
 
     getInterval(note1, note2) {
-	return TemperamentUtil.getNumericInterval(this.freqs, note1, note2);
+        return TemperamentUtil.getNumericInterval(this.freqs, note1, note2);
     }
 }
 
 class PythagorasTemperament {
 
     constructor() {
-	this.freqs = TemperamentUtil.scaleFromFractionalFifth(new Fraction(3, 2));
+        this.freqs = TemperamentUtil.scaleFromFractionalFifth(new Fraction(3, 2));
     }
 
     getInterval(note1, note2) {
-	return TemperamentUtil.getFractionalInterval(this.freqs, note1, note2);
+        return TemperamentUtil.getFractionalInterval(this.freqs, note1, note2);
     }
 
 }
@@ -148,11 +148,11 @@ class PythagorasTemperament {
 class QuarterCommaMeantoneTemperament {
 
     constructor() {
-	this.freqs = TemperamentUtil.scaleFromNumericFifth(Math.pow(5, 1/4));
+        this.freqs = TemperamentUtil.scaleFromNumericFifth(Math.pow(5, 1/4));
     }
 
     getInterval(note1, note2) {
-	return TemperamentUtil.getNumericInterval(this.freqs, note1, note2);
+        return TemperamentUtil.getNumericInterval(this.freqs, note1, note2);
     }
 
 }
@@ -160,23 +160,23 @@ class QuarterCommaMeantoneTemperament {
 class FiveLimitTuningTemperament {
 
     freqs = [
-	new Fraction( 1,  1),  // P1
-	new Fraction(16, 15),  // m2
-	new Fraction( 9,  8),  // M2
-	new Fraction( 6,  5),  // m3
- 	new Fraction( 5,  4),  // M3
- 	new Fraction( 4,  3),  // P4
-	new Fraction(45, 32),  // TT
- 	new Fraction( 3,  2),  // P5
-	new Fraction( 8,  5),  // m6
- 	new Fraction( 5,  3),  // M6
-	new Fraction(16,  9),  // m7
- 	new Fraction(15,  8),  // M7
+        new Fraction( 1,  1),  // P1
+        new Fraction(16, 15),  // m2
+        new Fraction( 9,  8),  // M2
+        new Fraction( 6,  5),  // m3
+        new Fraction( 5,  4),  // M3
+        new Fraction( 4,  3),  // P4
+        new Fraction(45, 32),  // TT
+        new Fraction( 3,  2),  // P5
+        new Fraction( 8,  5),  // m6
+        new Fraction( 5,  3),  // M6
+        new Fraction(16,  9),  // m7
+        new Fraction(15,  8),  // M7
     ];
 
     
     getInterval(note1, note2) {
-	return TemperamentUtil.getFractionalInterval(this.freqs, note1, note2);
+        return TemperamentUtil.getFractionalInterval(this.freqs, note1, note2);
     }
     
 }
@@ -184,23 +184,23 @@ class FiveLimitTuningTemperament {
 class SevenLimitTuningTemperament {
 
     freqs = [
-	new Fraction( 1,  1),  // P1
-	new Fraction(15, 14),  // m2
-	new Fraction( 9,  8),  // M2
-	new Fraction( 6,  5),  // m3
- 	new Fraction( 5,  4),  // M3
- 	new Fraction( 4,  3),  // P4
-	new Fraction( 7,  5),  // TT
- 	new Fraction( 3,  2),  // P5
-	new Fraction( 8,  5),  // m6
- 	new Fraction( 5,  3),  // M6
-	new Fraction( 7,  4),  // m7
- 	new Fraction(15,  8),  // M7
+        new Fraction( 1,  1),  // P1
+        new Fraction(15, 14),  // m2
+        new Fraction( 9,  8),  // M2
+        new Fraction( 6,  5),  // m3
+        new Fraction( 5,  4),  // M3
+        new Fraction( 4,  3),  // P4
+        new Fraction( 7,  5),  // TT
+        new Fraction( 3,  2),  // P5
+        new Fraction( 8,  5),  // m6
+        new Fraction( 5,  3),  // M6
+        new Fraction( 7,  4),  // m7
+        new Fraction(15,  8),  // M7
     ];
 
     
     getInterval(note1, note2) {
-	return TemperamentUtil.getFractionalInterval(this.freqs, note1, note2);
+        return TemperamentUtil.getFractionalInterval(this.freqs, note1, note2);
     }
     
 }
